51782ddca37eb5ed3361120b52ef09adbbbef188
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-06-03 16:04:57 +0530

    refactor: streamline merge_sorted_list method and improve display function

--- problem3.py
+++ problem3.py
@@ -1,5 +1,3 @@
-# Merge two sorted linkedlist into one large sorted LinkedList:
-
 class Node:
     def __init__(self, data):
         self.data = data
@@ -11,55 +9,55 @@ class LinkedList:
 
     def display(self):
         current = self.head
-        while current is not None:
-            print(current.data, end=' ->')
+        elements = []
+        while current:
+            elements.append(str(current.data))
             current = current.next
-        print("None")
+        print(" -> ".join(elements) + " -> None")
 
-    def merge_sorted_list(self, list1 , list2):
+    def merge_sorted_list(self, list1, list2):
         dummy = Node(0)
         current = dummy
-        while list1.head is not None and list2.head is not None:
-            if list1.head.data < list2.head.data:
-                current.next = list1.head
-                list1.head = list1.head.next
+        
+        # Track active pointers without destroying the original lists' heads
+        p1, p2 = list1.head, list2.head
+        
+        while p1 and p2:
+            if p1.data < p2.data:
+                current.next = p1
+                p1 = p1.next
             else:
-                current.next = list2.head
-                list2.head = list2.head.next
-
+                current.next = p2
+                p2 = p2.next
             current = current.next
             
-        if list1.head is not None:
-            current.next = list1.head
-        elif list2.head is not None:
-            current.next = list2.head
-            
+        # Clean up: Attach the remaining non-empty list segment
+        current.next = p1 if p1 else p2
+        
         self.head = dummy.next
         return self.head
-
-
-                
-        
-
-
-
-
+    
 
 if __name__ == "__main__":
     list1 = LinkedList()
     list1.head = Node(1)
-    list1.head.next = Node(2)
-    list1.head.next.next = Node(3)
-
+    list1.head.next = Node(3)
+    list1.head.next.next = Node(89)
     list2 = LinkedList()
     list2.head = Node(1)
-    list2.head.next = Node(3)
-    list2.head.next.next = Node(4)
+    list2.head.next = Node(6)
+    list2.head.next.next = Node(9)
 
 
+    print("list1")
     list1.display()
+    print("list2")
     list2.display()
+    
+    merge_list = LinkedList()
+    merge_list.merge_sorted_list(list1,list2)
+    print("Merged List")
+    merge_list.display()
 
-    merged_list = LinkedList()
-    merged_list.merge_sorted_list(list1,list2)
-    merged_list.display()
+
+
