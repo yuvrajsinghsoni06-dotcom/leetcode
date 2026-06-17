3e9497df28f30433f989b756951080bc82e74e37
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-06-17 16:01:42 +0530

    feat: refactor LinkedList class and remove unused HCF function

--- problem23.py
+++ problem23.py
@@ -1,14 +1,56 @@
 
 
-def hcf(p1,p2):
-    hcf = 1
-    larger = p1 if p1 > p2 else p2
-    for i in range(1,larger):
-        if ((p1 % i == 0) and (p2 % i == 0)):
-            hcf = i
-    return hcf
-    
-p1 = 10
-p2 = 6
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
 
-print(hcf(p1,p2))
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def display(self):
+        current = self.head
+        element = []
+        while current:
+            element.append(str(current.val))
+            current = current.next
+
+        print(" -> ".join(element) + " -> None")
+
+    def solution(self):
+        if not self.head or not self.head.next:
+            return self.head
+        slow = self.head
+        fast = slow.next
+        total_sum = 0
+        while fast:
+            if fast.val != 0:
+                total_sum += fast.val
+            else:
+                slow = slow.next
+                slow.val = total_sum
+                total_sum = 0
+
+            fast = fast.next
+        slow.next = None
+        self.head = self.head.next
+        return self.head
+    
+if __name__ == "__main__":
+    llist = LinkedList()
+    llist.head = Node(0)
+    llist.head.next = Node(3)
+    llist.head.next.next = Node(1)
+    llist.head.next.next.next = Node(0)
+    llist.head.next.next.next.next = Node(4)
+    llist.head.next.next.next.next.next = Node(5)
+    llist.head.next.next.next.next.next.next = Node(2)
+    llist.head.next.next.next.next.next.next.next = Node(0)
+
+
+    llist.display()
+    llist.solution()
+    llist.display()
+
+
