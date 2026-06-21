afa3adaeb2e4dfd975043683d3959d3d8f6c6cbb
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-06-21 16:10:53 +0530

    fix: optimize sorting method for costs in totalice_cream function

--- problem27.py
+++ problem27.py
@@ -4,13 +4,15 @@ class Solution:
     def totalice_cream(self, costs: list[int],coins : int):
         ice_cream = 0
         n = len(costs)
-        costs = sorted(costs)
+        costs.sort()
         if not costs :
             return ice_cream
         for cost in costs:
             if coins >= cost:
                 coins -= cost
                 ice_cream += 1
+            else:
+                break
         return ice_cream
 
     
