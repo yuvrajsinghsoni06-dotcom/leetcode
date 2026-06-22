d4120f6bb91ef2d6fee69782294bcafcc86457b4
yuvrajsonigreat <yuvrajsinghsoni06@gmail.com>
2026-06-22 17:42:41 +0530

    feat: implement maxBalloon method to count maximum number of balloons in a string

--- problem28.py
+++ problem28.py
@@ -1,6 +1,28 @@
 # 1189. Maximum Number of Balloons
 
 
-text = "balloon"
-for i in text:
-    print(i)
+class Solution:
+    def maxBalloon(self,text: str):
+        if not text:
+           return 0
+        count = 0
+        collection = ["b","a","l","l","o","o","n"]
+        for char in text:
+            if char in collection:
+                collection.remove(char)
+            else:
+                continue
+        if len(collection) == 0:
+               count +=1
+        else:
+            return 
+        return count
+
+
+if __name__ == "__main__":
+   sol = Solution()
+   text = "loonbalxballpoon"
+   result = sol.maxBalloon(text)
+   print(result)
+        
+
