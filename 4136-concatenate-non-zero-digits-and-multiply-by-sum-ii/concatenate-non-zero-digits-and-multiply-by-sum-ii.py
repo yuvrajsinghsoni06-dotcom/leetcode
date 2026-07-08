784a9c7baa83a0f89e936335ff6892da5831b8d7
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-07-08 14:55:15 +0530

    Time: 464 ms (66.67%) | Memory: 57.6 MB (33.33%) - LeetSync

--- 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
+++ 4136-concatenate-non-zero-digits-and-multiply-by-sum-ii/concatenate-non-zero-digits-and-multiply-by-sum-ii.py
@@ -1,90 +1,38 @@
 class Solution:
-    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
-        MOD = 10**9 + 7
+    def __getattr__(self, name):
+        return self.solve
+        
+    def solve(self, s: str, queries: list[list[int]]) -> list[int]:
         n = len(s)
-
+        MOD = 10**9 + 7
+        
+        pref_val = [0] * (n + 1)
+        pref_sum = [0] * (n + 1)
+        cnt = [0] * (n + 1)
+        
         pow10 = [1] * (n + 1)
         for i in range(1, n + 1):
             pow10[i] = (pow10[i - 1] * 10) % MOD
-class Solution:
-    def sumAndMultiply(self, s: str, queries: List[List[int]]) -> List[int]:
-        n = len(s)
-        m = len(queries)
-        MOD = 10**9 + 7
-
-        prefSum = [0] * (n + 1)
-        prefVal = [0] * (n + 1)
-        prefCnt = [0] * (n + 1)
-        power = [1] * (n + 1)
-
-        for i in range(1, n + 1):
-            power[i] = (power[i - 1] * 10) % MOD
-
+            
         for i in range(n):
-            d = int(s[i])
-
-            prefSum[i + 1] = prefSum[i] + d
-            prefCnt[i + 1] = prefCnt[i] + (1 if d != 0 else 0)
-
-            if d == 0:
-                prefVal[i + 1] = prefVal[i]
+            val = int(s[i])
+            if val > 0:
+                pref_val[i + 1] = (pref_val[i] * 10 + val) % MOD
+                pref_sum[i + 1] = pref_sum[i] + val
+                cnt[i + 1] = cnt[i] + 1
             else:
-                prefVal[i + 1] = (prefVal[i] * 10 + d) % MOD
-
-        result = [0] * m
-
-        for i in range(m):
-            l, r = queries[i]
-
-            length = prefCnt[r + 1] - prefCnt[l]
-
-            start = prefVal[l]
-            end = prefVal[r + 1]
-
-            x = (end - (start * power[length]) % MOD + MOD) % MOD
-            sm = prefSum[r + 1] - prefSum[l]
-
-            result[i] = (x * sm) % MOD
-
-        return result
-        # idx[i] = number of non-zero digits before index i
-        idx = [0] * (n + 1)
-
-        # val[i] = number formed by first i non-zero digits
-        val = [0] * (n + 1)
-
-        # total[i] = sum of first i non-zero digits
-        total = [0] * (n + 1)
-
-        cnt = 0
-
-        for i, ch in enumerate(s):
-            digit = int(ch)
-
-            if digit != 0:
-                cnt += 1
-                val[cnt] = (val[cnt - 1] * 10 + digit) % MOD
-                total[cnt] = total[cnt - 1] + digit
-
-            idx[i + 1] = cnt
-
+                pref_val[i + 1] = pref_val[i]
+                pref_sum[i + 1] = pref_sum[i]
+                cnt[i + 1] = cnt[i]
+                
         ans = []
-
         for l, r in queries:
-
-            left = idx[l]
-            right = idx[r + 1]
-
-            # No non-zero digit in the range
-            if left == right:
+            c = cnt[r + 1] - cnt[l]
+            if c == 0:
                 ans.append(0)
-                continue
-
-            length = right - left
-
-            number = (val[right] - val[left] * pow10[length]) % MOD
-            sum_digits = total[right] - total[left]
-
-            ans.append((number * sum_digits) % MOD)
-
+            else:
+                s_val = pref_sum[r + 1] - pref_sum[l]
+                v_val = (pref_val[r + 1] - pref_val[l] * pow10[c]) % MOD
+                ans.append((v_val * s_val) % MOD)
+                
         return ans
