f6004f4919bd9703e74a1ebe3206f2bef3c0650d
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-07-11 14:05:23 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (99.77%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,9 +1,15 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
         count = 0
-        for i , num in enumerate(nums):
-            for j in range(i+1,len(nums)):
-                if num == nums[j]:
-                    count +=1
-        return count
+        counts_map = {}  # This is our hash map
         
+        for num in nums:
+            # If we've seen this number before, add its current frequency to our pairs
+            if num in counts_map:
+                count += counts_map[num]
+                counts_map[num] += 1
+            else:
+                # First time seeing this number
+                counts_map[num] = 1
+                
+        return count
