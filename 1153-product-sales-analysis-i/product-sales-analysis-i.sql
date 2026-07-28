c07fda2431301f84de99f95a707ef73a382d54c1
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-07-28 11:31:27 +0530

    Time: 1295 ms (31.12%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 SELECT p.product_name , 
-s.year , s.price 
+s.year , price 
 from Sales s 
-inner join Product p
+left join Product p
 on s.product_id = p.product_id;
