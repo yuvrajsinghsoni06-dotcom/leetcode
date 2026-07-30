3a350d195ddda66654a0f45294cae23eabb40e15
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-07-30 16:20:46 +0530

    Time: 239 ms (68.42%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,12 +1,19 @@
-SELECT 
+SELECT
     s.user_id,
     ROUND(
-        COALESCE(
-            AVG(CASE WHEN c.action = 'confirmed' THEN 1.0 ELSE 0.0 END), 
-            0.00
-        ), 
+        AVG(
+            CASE 
+                WHEN c.action = 'confirmed' THEN 1.00
+                ELSE 0
+            END
+        ),
         2
-    ) AS confirmation_rate
-FROM Signups s
-LEFT JOIN Confirmations c ON s.user_id = c.user_id
-GROUP BY s.user_id;
+    ) confirmation_rate
+FROM
+    Signups s
+LEFT JOIN
+    Confirmations c
+    ON
+    s.user_id = c.user_id
+GROUP BY
+    s.user_id
