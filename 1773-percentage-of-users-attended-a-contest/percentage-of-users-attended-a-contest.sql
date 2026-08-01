cece778c9da3f6aa6ddece76bb093d5a84093070
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-01 13:14:24 +0530

    Time: 992 ms (47.30%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,7 @@
+select contest_id,
+      round(count(user_id) * 100.0 /  (select count(*) from Users),2) as percentage
+from Register
+group by contest_id
+order by 
+       percentage desc,
+       contest_id asc;
