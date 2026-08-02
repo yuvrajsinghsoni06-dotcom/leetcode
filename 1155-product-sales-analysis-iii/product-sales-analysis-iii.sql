a6fa908517af4f4aa95e2eb1885c977569ea3320
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-02 15:04:21 +0530

    Time: 887 ms (67.47%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,11 +1,16 @@
-SELECT product_id,
-year as first_year,
-quantity, 
-price
-from Sales
-where(product_id, year) in (
-    select product_id , min(year)
-    from Sales
-    group by product_id
-);
-
+WITH RankedSales AS (
+    SELECT 
+        product_id,
+        year AS first_year,
+        quantity,
+        price,
+        RANK() OVER (PARTITION BY product_id ORDER BY year ASC) AS rk
+    FROM Sales
+)
+SELECT 
+    product_id, 
+    first_year, 
+    quantity, 
+    price
+FROM RankedSales
+WHERE rk = 1;
