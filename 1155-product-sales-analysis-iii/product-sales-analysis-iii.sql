837bfefcc6cd6ba8215bf8389b224228f9ccddca
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-02 14:58:47 +0530

    Time: 1038 ms (22.84%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,11 @@
+SELECT product_id,
+year as first_year,
+quantity, 
+price
+from Sales
+where(product_id, year) in (
+    select product_id , min(year)
+    from Sales
+    group by product_id
+);
+
