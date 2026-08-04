79fe5f180cf1b2ce812d8da0df8055d0af2e3923
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-04 19:58:55 +0530

    Time: 3 ms (69.59%) | Memory: 19.2 MB (55.74%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,6 +1,11 @@
 class Solution:
     def findMissingElements(self, nums: List[int]) -> List[int]:
-        num_set = set(nums)
-        x = min(nums)
-        y = max(nums)
-        return [x for x in range(x, y + 1) if x not in num_set]
+        nums.sort()
+        ans = []
+        
+        # Iterate through adjacent pairs and fill in any missing gaps
+        for i in range(len(nums) - 1):
+            for missing in range(nums[i] + 1, nums[i + 1]):
+                ans.append(missing)
+                
+        return ans
