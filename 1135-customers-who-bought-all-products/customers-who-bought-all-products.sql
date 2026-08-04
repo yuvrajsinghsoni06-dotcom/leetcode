84b51e70a70be7905a07c73efa8f46de270c56ba
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-04 20:29:25 +0530

    Time: 551 ms (33.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+select c.customer_id
+from customer c
+group by c.customer_id
+having count(distinct c.product_key) = (select count(product_key) from product);
+
