a088d4287f9cec32630046f305145eee0aa0b7b6
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-06 13:54:47 +0530

    Time: 273 ms (49.94%) | Memory: 0B (100.00%) - LeetSync

--- 180-consecutive-numbers/consecutive-numbers.sql
+++ 180-consecutive-numbers/consecutive-numbers.sql
@@ -1,13 +1,13 @@
 -- Write your PostgreSQL query statement below
-select distinct num as ConsecutiveNums
-from (
+with ranklogs as (
     select num,
     lead(num,1) over(order by id) as next_num,
-    lag(num,1) over (order by id) as prev_num
+    lag(num,1) over(order by id ) as prev_num
     from logs
-) as ranklogs
-where num = next_num and num = prev_num
-
+)
+select distinct num as ConsecutiveNums
+from ranklogs
+where num = next_num and num = prev_num;
 
 
 
