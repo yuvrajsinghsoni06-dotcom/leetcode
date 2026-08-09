8f823b872fdb036cb834104e1e5721b9bfcad7ba
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-09 12:20:00 +0530

    Time: 3204 ms (94.98%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -0,0 +1,32 @@
+-- Write your PostgreSQL query statement bel
+
+
+select 'Low Salary'
+as category ,
+count(*) as accounts_count
+from accounts 
+where income < 20000
+
+union all
+
+select 'Average Salary'
+as category ,
+count(*) as accounts_count from accounts 
+where income >= 20000
+and income <= 50000
+
+union all
+
+select 'High Salary' 
+as category ,
+count(*) as accounts_count from accounts 
+where income > 50000;
+
+
+
+
+
+
+
+
+
