56b5c8a4ce7a9b62db9120984ba8abc3ee4c5866
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-11 19:27:07 +0530

    Time: 214 ms (20.76%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,8 +1,7 @@
-select 
-case 
-when id % 2 != 0 and id = (select max(id) from seat) then 
-id
-when id % 2 != 0 then id + 1
-else id -1 end as id, student
-from seat
-order by id; 
+SELECT 
+    id,
+    CASE 
+        WHEN id % 2 = 0 THEN LAG(student) OVER (ORDER BY id)
+        ELSE COALESCE(LEAD(student) OVER (ORDER BY id), student)
+    END AS student
+FROM Seat;
