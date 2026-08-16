474dc7b2c60155175da462f16aa2c17bb19f00eb
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-16 17:18:04 +0530

    Time: 315 ms (77.66%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,17 @@
-select a.employee_id,
-a.name,
-count(b.reports_to) as reports_count,
-round(avg(b.age), 0) as average_age
-from employees a
-inner join employees b
-      on a.employee_id = b.reports_to
-group by a.employee_id,
-         a.name
-order by a.employee_id;
+SELECT 
+    e.employee_id,
+    e.name,
+    r.reports_count,
+    r.average_age
+FROM (
+    SELECT 
+        reports_to,
+        COUNT(*) AS reports_count,
+        ROUND(AVG(age)) AS average_age
+    FROM employees
+    WHERE reports_to IS NOT NULL
+    GROUP BY reports_to
+) r
+JOIN employees e 
+    ON e.employee_id = r.reports_to
+ORDER BY e.employee_id;
