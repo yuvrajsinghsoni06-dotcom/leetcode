b6bd29890282cff0590900433b01600961dcbf28
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-17 20:26:41 +0530

    Time: 247 ms (39.02%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,9 +1,17 @@
+-- All employees with their explicitly marked primary department
 SELECT employee_id, department_id
 FROM employee
 WHERE primary_flag = 'Y'
-   OR employee_id IN (
-       SELECT employee_id
-       FROM employee
-       GROUP BY employee_id
-       HAVING COUNT(department_id) = 1
-   );
+
+UNION
+
+-- All employees who belong to only 1 department
+SELECT employee_id, department_id
+FROM employee
+GROUP BY employee_id, department_id
+HAVING employee_id IN (
+    SELECT employee_id
+    FROM employee
+    GROUP BY employee_id
+    HAVING COUNT(department_id) = 1
+);
