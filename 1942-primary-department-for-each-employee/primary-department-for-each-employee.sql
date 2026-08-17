e8803c94fc7c56c3631b0e4a3d567fb347c5a058
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-17 20:26:27 +0530

    Time: 360 ms (5.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -0,0 +1,9 @@
+SELECT employee_id, department_id
+FROM employee
+WHERE primary_flag = 'Y'
+   OR employee_id IN (
+       SELECT employee_id
+       FROM employee
+       GROUP BY employee_id
+       HAVING COUNT(department_id) = 1
+   );
