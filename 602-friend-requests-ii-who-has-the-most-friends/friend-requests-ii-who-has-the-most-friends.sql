d84fc5ddd14c137ca1c5c252e1c8f052fa5bb22f
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-23 17:17:29 +0530

    Time: 198 ms (32.65%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,11 +1,11 @@
-with friendship as(
-    select requester_id as id from requestaccepted
-    union all
-    select accepter_id as id from requestaccepted
-)
-select id , 
-count(*) as num
-from friendship
-group by id
-order by num desc
-limit 1;
+SELECT 
+    id,
+    COUNT(*) AS num
+FROM (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+) AS all_friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
