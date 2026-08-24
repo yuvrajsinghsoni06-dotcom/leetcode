63de27949d821cf6070f30699c652bb1a6cee041
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-08-24 13:53:21 +0530

    Time: 290 ms (33.71%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,17 @@
+-- Write your PostgreSQL query statement below
+
+select round(cast(sum(tiv_2016) as numeric),2) as tiv_2016
+from insurance
+where tiv_2015 in (
+    select tiv_2015 
+    from insurance
+    group by tiv_2015
+    having count(*) > 1
+)
+and (lat , lon) in (
+    select lat, lon
+    from insurance
+    group by lat ,lon
+    having count(*) = 1
+
+);
