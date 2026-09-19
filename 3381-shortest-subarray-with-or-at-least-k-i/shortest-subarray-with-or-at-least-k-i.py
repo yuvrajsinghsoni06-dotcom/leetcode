7cb89fa3f08c7f6ceaffa3ea052d02de486ff8f6
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-09-19 17:19:10 +0530

    Time: 8 ms (37.82%) | Memory: 19.4 MB (8.82%) - LeetSync

--- 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.py
+++ 3381-shortest-subarray-with-or-at-least-k-i/shortest-subarray-with-or-at-least-k-i.py
@@ -1,17 +1,12 @@
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        min_str = float('inf')
         n = len(nums)
-        min_len = float('inf')
-        
-        # Check all possible starting indices
         for i in range(n):
-            current_or = 0
-            # Expand the subarray from index i to j
-            for j in range(i, n):
-                current_or |= nums[j]  # Bitwise OR update
-                
-                if current_or >= k:
-                    min_len = min(min_len, j - i + 1)
-                    break  # Expanding further from 'i' will only increase length
-                    
-        return min_len if min_len != float('inf') else -1
+            current = 0
+            for j in range(i,n):
+                current |= nums[j]
+                if current >= k:
+                    min_str =min(min_str,j - i + 1)
+        return min_str if min_str != float('inf') else -1
+        
