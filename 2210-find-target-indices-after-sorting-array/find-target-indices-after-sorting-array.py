28587140468504de199e77a43ce752568c9f83a0
Emperor_Yuvraj <yuvrajsinghsoni06@gmail.com>
2026-09-23 19:30:15 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (91.72%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -1,7 +1,6 @@
 class Solution:
     def targetIndices(self, nums: list[int], target: int) -> list[int]:
         ans = []
-        n = len(nums)
         nums.sort()
         for i,num in enumerate(nums):
             if num == target:
